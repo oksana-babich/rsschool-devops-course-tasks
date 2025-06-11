5fab7b622abb6325ddee50da0abb16086cf45320
Aksana Babich <anikejoksana@gmail.com>
2025-06-11 18:20:26 +0100

    Add lifecycle block to S3 bucket resource to prevent destruction

--- task_1/S3Bucket_Creation_ForRemoteState.tf
+++ task_1/S3Bucket_Creation_ForRemoteState.tf
@@ -2,6 +2,9 @@
 resource "aws_s3_bucket" "remote_state" {
   bucket = var.bucket_name
 
+  lifecycle {
+    prevent_destroy = true
+  }
 
   tags = {
     Name  = "Remote State Bucket for DevOps Course"
