3b9d9e8d83f82a935ceda922a947565a5067c160
Aksana Babich <anikejoksana@gmail.com>
2025-06-21 15:45:03 +0100

    Remove unnecessary Terraform provider block from vpc.tf for cleaner configuration

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.81.0"
-    }
-  }
-}
 resource "aws_vpc" "DevOps_course_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
